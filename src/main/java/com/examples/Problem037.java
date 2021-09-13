61e9f698e78a4fea95e319ee3c9924ea291817e4
NatxoFerro <natxoferro@gmail.com>
2021-09-13 19:24:32 +0200

    Problem037 002 List + Array

--- src/main/java/com/examples/Problem037.java
+++ src/main/java/com/examples/Problem037.java
@@ -8,33 +8,56 @@ public class Problem037 {
 
 	public static void main(String[] args) {
 		Problem037 test = new Problem037();
-		System.out.println(Arrays.asList(test.generateSet(0)) + " -> " + test.checkPowerSet(test.generateSet(0)));
-		System.out.println(Arrays.asList(test.generateSet(1)) + " -> " + test.checkPowerSet(test.generateSet(1)));
-		System.out.println(Arrays.asList(test.generateSet(2)) + " -> " + test.checkPowerSet(test.generateSet(2)));
-		System.out.println(Arrays.asList(test.generateSet(3)) + " -> " + test.checkPowerSet(test.generateSet(3)));
-		System.out.println(Arrays.asList(test.generateSet(4)) + " -> " + test.checkPowerSet(test.generateSet(4)));
+		System.out.println(Arrays.asList(test.generateSet(0)) + " -> " + test.generatePowerSet(test.generateSet(0)));
+		System.out.println(Arrays.asList(test.generateSet(1)) + " -> " + test.generatePowerSet(test.generateSet(1)));
+		System.out.println(Arrays.asList(test.generateSet(2)) + " -> " + test.generatePowerSet(test.generateSet(2)));
+		System.out.println(Arrays.asList(test.generateSet(3)) + " -> " + test.generatePowerSet(test.generateSet(3)));
+		System.out.println(Arrays.asList(test.generateSet(4)) + " -> " + test.generatePowerSet(test.generateSet(4)));
+		System.out.println(test.generateList(0) + " -> " + test.generatePowerSet(test.generateList(0)));
+		System.out.println(test.generateList(1) + " -> " + test.generatePowerSet(test.generateList(1)));
+		System.out.println(test.generateList(2) + " -> " + test.generatePowerSet(test.generateList(2)));
+		System.out.println(test.generateList(3) + " -> " + test.generatePowerSet(test.generateList(3)));
+		System.out.println(test.generateList(4) + " -> " + test.generatePowerSet(test.generateList(4)));
 	}
 
-	List<List<Integer>> checkPowerSet(Integer[] set) {
-		List<Integer> in = new ArrayList<Integer>(0);
+	List<List<Integer>> generatePowerSet(Integer[] set) {
+		return generatePowerSet(set, set.length);
+	}
+	
+	private List<List<Integer>> generatePowerSet(Integer[] set, int lastItemIndex) {
 		List<List<Integer>> out = new ArrayList<List<Integer>>(0);
-		out.add(new ArrayList<Integer>(0));
-		for (int item = 0; item < set.length; item++) {
-			out.addAll(checkCombinations(in, set));
-			in.add(set[item]);
+		if (lastItemIndex == 0) {
+			out.add(new ArrayList<Integer>(0));
+		} else {
+			int lastItem = set[lastItemIndex - 1];
+			out = generatePowerSet(set, lastItemIndex - 1);
+			List<List<Integer>> copy = new ArrayList<List<Integer>>(0);
+			out.forEach(item -> copy.add(new ArrayList<Integer>(item)));
+			for (List<Integer> data : copy) {
+				data.add(lastItem);
+				out.add(data);
+			}
+			out.sort((item1, item2) -> item1.size() - item2.size());
 		}
 		return out;
 	}
 
-	List<List<Integer>> checkCombinations(List<Integer> array, Integer[] set) {
-		List<List<Integer>> result = new ArrayList<List<Integer>>(0);
-		for (int item = array.size(); item < set.length; item++) {
-			List<Integer> partial = new ArrayList<Integer>(0);
-			partial.addAll(array);
-			partial.add(set[item]);
-			result.add(partial);
+	List<List<Integer>> generatePowerSet(List<Integer> set) {
+		List<List<Integer>> out = new ArrayList<List<Integer>>(0);
+		if (set.size() == 0) {
+			out.add(new ArrayList<Integer>(0));
+		} else {
+			int lastItem = set.remove(set.size() - 1);
+			out = generatePowerSet(set);
+			List<List<Integer>> copy = new ArrayList<List<Integer>>(0);
+			out.forEach(item -> copy.add(new ArrayList<Integer>(item)));
+			for (List<Integer> data : copy) {
+				data.add(lastItem);
+				out.add(data);
+			}
+			out.sort((item1, item2) -> item1.size() - item2.size());
 		}
-		return result;
+		return out;
 	}
 
 	Integer[] generateSet(int setSize) {
@@ -42,6 +65,13 @@ public class Problem037 {
 		for (int counter = 1; counter <= setSize; counter++)
 			result.add(counter);
 		return result.toArray(new Integer[result.size()]);
+	}
+
+	List<Integer> generateList(int setSize) {
+		List<Integer> result = new ArrayList<Integer>(0);
+		for (int counter = 1; counter <= setSize; counter++)
+			result.add(counter);
+		return result;
 	}
 
 }
